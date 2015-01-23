832afd7bf05f5140cc56bfbd74d7c1ec61c7158b
Evan Humphries <humphries.evan@gmail.com>
2015-01-23 13:41:17 -0500

    fixed site url and email settings in app

--- app/config/mail.php
+++ app/config/mail.php
@@ -54,7 +54,7 @@ return array(
 	|
 	*/
 
-	'from' => array('address' => 'hello@ehumps.me', 'name' => 'ehumps'),
+	'from' => array('address' => 'hello@ehumps.me', 'name' => 'Plexy'),
 
 	/*
 	|--------------------------------------------------------------------------
