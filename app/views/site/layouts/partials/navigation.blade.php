8f4ed32401ad9f4c36c5ca6ccc13f32f6160a828
Jordan Stewart <jstewart@weblinc.com>
2015-03-21 17:55:49 -0400

    WIP nav layout/functionality

--- app/views/site/layouts/partials/navigation.blade.php
+++ app/views/site/layouts/partials/navigation.blade.php
@@ -4,33 +4,44 @@
 		<div class="container">
 				<!-- Brand and toggle get grouped for better mobile display -->
 				<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+						<button type="button" class="navbar-toggle"
+							data-toggle="collapse"
+							data-target="#account">
 								<span class="sr-only">Toggle navigation</span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Plexy</a>
+						<button type="button" class="navbar-toggle"
+							data-toggle="collapse"
+							data-target="#search">
+								<span class="sr-only">Search</span>
+								<span class="glyphicon glyphicon-search"></span>
+						</button>
+						<a class="navbar-brand" href="/">Plexy</a>
 				</div>
+
+				<!-- Search -->
+				<div id="search" class="navbar_search collapse navbar-collapse">
+					@include('site/layouts/partials/search')
+				</div>
+
+				<!-- Account login -->
 				<!-- Collect the nav links, forms, and other content for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
-								<li{{ (Request::is('/') ? ' class="active"' : '') }}>
-										<a href="{{{ URL::to('/') }}}">Home</a>
-								</li>
-						</ul>
-						<ul class="nav navbar-nav pull-right">
-							<li class="dropdown">
-								<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-									{{{ Auth::user()->username }}}	<span class="caret"></span>
-								</a>
-								<ul class="dropdown-menu">
-									<li><a href="{{{ URL::to('user/settings') }}}">Settings</a></li>
-									<li class="divider"></li>
-									<li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
-								</ul>
-							</li>
-						</ul>
+				<div id="account" class="navbar_account collapse navbar-collapse" >
+					<ul class="nav navbar-nav" id="account">
+						<li class="dropdown">
+							<a class="dropdown-toggle" data-toggle="dropdown" href="#">
+								{{{ Auth::user()->username }}}
+								<span class="caret"></span>
+							</a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="{{{ URL::to('user/settings') }}}">Settings</a></li>
+								<li><a href="{{{ URL::to('user/logout') }}}">Logout</a></li>
+								<li><a href="{{{ URL::to('#') }}}">other</a></li>
+							</ul>
+						</li>
+					</ul>
 				</div>
 				<!-- /.navbar-collapse -->
 		</div>
@@ -47,15 +58,15 @@
 								<span class="icon-bar"></span>
 								<span class="icon-bar"></span>
 						</button>
-						<a class="navbar-brand" href="#">Plexy</a>
+						<a class="navbar-brand" href="/">Plexy</a>
 				</div>
 				<!-- Collect the nav links, forms, and other content for toggling -->
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<ul class="nav navbar-nav">
+						<!-- <ul class="nav navbar-nav">
 								<li{{ (Request::is('/') ? ' class="active"' : '') }}>
-										<a href="{{{ URL::to('/') }}}">Home</a>
+										<a href="{{{ URL::to('/') }}}">Plexy</a>
 								</li>
-						</ul>
+						</ul> -->
 						<ul class="nav navbar-nav pull-right">
 								@if (Auth::check())
 								<li><a href="{{{ URL::to('user') }}}">Logged in as {{{ Auth::user()->username }}}</a></li>
